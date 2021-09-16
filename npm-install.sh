3bf9d06863c0becfd1b647804c7f7d537455dbea
Tobias Schifftner <ts@ambimax.de>
2021-09-16 09:56:20 +0200

    fix: updated semantic release packages

--- npm-install.sh
+++ npm-install.sh
@@ -7,11 +7,11 @@ echo -e "//npm.pkg.github.com/:_authToken=$(cat /run/secrets/GITHUB_TOKEN)\n \
 
 npm install --global \
     @ambimax/semantic-release-composer@1.0.2 \
-    semantic-release@v17.3.7 \
+    semantic-release@v17.4.7 \
     @semantic-release/changelog@^5.0.1 \
     @semantic-release/exec@^5.0.0 \
     @semantic-release/commit-analyzer@^8.0.1 \
     semantic-release-docker@v2.2.0 \
-    @semantic-release/git@^9.0.0 \
-    @semantic-release/github@^7.2.0 \
-    @semantic-release/release-notes-generator@^9.0.1
+    @semantic-release/git@^9.0.1 \
+    @semantic-release/github@^7.2.3 \
+    @semantic-release/release-notes-generator@^9.0.3
